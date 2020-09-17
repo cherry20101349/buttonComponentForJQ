08e743da1973ae6b54f415cf8ebea28dcbdb5bd9
wuyp <wuyp@fsmeeting.com>
2020-09-17 10:40:38 +0800

    使用render函数替换template

--- button.js
+++ button.js
@@ -1,4 +1,18 @@
 Vue.component('el-button', {
+    render: function (h) {
+        return h('button',
+            {
+                'class': [{'is-loading': this.loading}, this.type, this.size, 'el-button'],
+                on: {
+                    click: this.clickBtn
+                }
+            },
+            [
+                this.loading || this.icon ? h('i', {}) : '',
+                this.$slots.default
+            ]
+        )
+    },
     props: {
         // 按钮图标
         icon: {
@@ -21,7 +35,6 @@ Vue.component('el-button', {
             default: false
         }
     },
-    template: '<button class="el-button" :class="[{\'is-loading\' : loading}, type, size]" @click="clickBtn"><i v-if="icon" :class="icon"></i><slot></slot></button>',
     methods: {
         clickBtn: function () {
             // this.$emit方法不能使用驼峰命名的函数
